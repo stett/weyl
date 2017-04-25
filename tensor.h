033d2a42662706f228b54d99b262c3a15d39a8bc
Stett <stettjawa@gmail.com>
2017-04-25 01:24:55 -0700

    tensor dimension count

--- tensor.h
+++ tensor.h
@@ -17,6 +17,18 @@ namespace weyl
         {
             enum { value = N0 };
         };
+
+        template <size_t N0, size_t... N>
+        struct Dimensions
+        {
+            enum { count = 1 + (int)Dimensions<N...>::count };
+        };
+
+        template <size_t N>
+        struct Dimensions<N>
+        {
+            enum { count = 1 };
+        };
     }
 
     template <typename T, size_t N0, size_t... N>
@@ -27,6 +39,8 @@ namespace weyl
         /// \brief Get the "size" of the Ith dimension of the tensor.
         template <size_t I>
         using dimension = detail::Dimension<I, N0, N...>;
+
+        using dimensions = detail::Dimensions<N0, N...>;
 
         using initializer = typename std::initializer_list< typename tensor<T, N...>::initializer >;
 
@@ -40,14 +54,14 @@ namespace weyl
             std::copy(values.begin(), values.end(), data);
         }
 
-        /*
         /// \brief Produce a tensor product by summing up dimension I
         /// of this tensor with dimension J of the other.
-        template <size_t I, size_t J, typename OtherT>
-        auto sum() {
-            static_assert(dimension<I>::value == OtherT::dimension<J>::value);
+        template <size_t I, size_t J, size_t M0, size_t... M>
+        auto sum(const tensor<T, M0, M...>& other) {
+            static_assert(
+                (int)dimension<I>::value == (int)detail::Dimension<J, M0, M...>::value,
+                "Indexes over which to sum must have equal dimensionality between tensors.");
         }
-        */
 
         tensor<T, N...>& operator[](size_t i) {
             return data[i];
@@ -72,6 +86,8 @@ namespace weyl
         template <size_t I>
         using dimension = detail::Dimension<I, N>;
 
+        using dimensions = detail::Dimensions<N>;
+
         using initializer = std::initializer_list<T>;
 
         tensor() {}
@@ -83,6 +99,30 @@ namespace weyl
         tensor(const initializer& values) {
             std::copy(values.begin(), values.end(), data);
         }
+
+        template <typename... Args>
+        tensor(Args... args) : data{ args... } {}
+
+        /*
+        /// \brief Produce a tensor product by summing up dimension I
+        /// of this tensor with dimension J of the other.
+        template <size_t I, size_t J, size_t M0, size_t... M>
+        auto sum(const tensor<T, M0, M...>& other) {
+            static_assert(I == 0);
+            static_assert(
+                (int)dimension<I>::value == (int)detail::Dimension<J, M0, M...>::value,
+                "Indexes over which to sum must have equal dimensionality between tensors.");
+
+            //
+            // int dim = Dimensions<M0, M...>::count;
+            // tensor<T, M[0], M[1], ..., M[J], ..., M[dim]> result;
+            //
+
+            for (size_t n = 0; n < N; ++n) {
+
+            }
+        }
+        */
 
         T& operator[](size_t i) {
             return data[i];
--- test_tensor.cpp
+++ test_tensor.cpp
@@ -3,7 +3,7 @@
 using namespace weyl;
 
 TEST_CASE("Tensor default constructor", "[tensor]") {
-    tensor<float, 2, 3, 1> t0;
+    tensor<float, 2, 3, 1> t;
 }
 
 TEST_CASE("Tensor single-value constructor", "[tensor]") {
@@ -16,11 +16,7 @@ TEST_CASE("Tensor single-value constructor", "[tensor]") {
 }
 
 TEST_CASE("Tensor initializer-list constructor", "[tensor]") {
-    tensor<float, 2, 3> t({
-        { 1.0f, 2.0f, 3.0f },
-        { 4.0f, 5.0f, 6.0f }
-    });
-
+    tensor<float, 2, 3> t({ { 1.0f, 2.0f, 3.0f }, { 4.0f, 5.0f, 6.0f } });
     REQUIRE(t[0][0] == 1.0f);
     REQUIRE(t[0][1] == 2.0f);
     REQUIRE(t[0][2] == 3.0f);
@@ -29,9 +25,33 @@ TEST_CASE("Tensor initializer-list constructor", "[tensor]") {
     REQUIRE(t[1][2] == 6.0f);
 }
 
+TEST_CASE("Single-index tensor variadic constructor", "[tensor]") {
+    tensor<float, 3> t(1.0f, 2.0f, 3.0f);
+    REQUIRE(t[0] == 1.0f);
+    REQUIRE(t[1] == 2.0f);
+    REQUIRE(t[2] == 3.0f);
+}
+
 TEST_CASE("Tensor dimensionality", "[tensor]") {
     using T = tensor<float, 2, 3, 1>;
+    REQUIRE(T::dimensions::count == 3);
     REQUIRE(T::dimension<0>::value == 2);
     REQUIRE(T::dimension<1>::value == 3);
     REQUIRE(T::dimension<2>::value == 1);
+}
+
+TEST_CASE("Correct tensor summations are callable", "[tensor]") {
+    tensor<float, 2, 3, 1> t1;
+    tensor<float, 4, 2, 3> t2;
+    t1.sum<0, 1>(t2);
+    t1.sum<1, 2>(t2);
+    t2.sum<1, 0>(t1);
+    t2.sum<2, 1>(t1);
+}
+
+TEST_CASE("Single index tensor summation", "[tensor]") {
+    using T = tensor<float, 3>;
+    T t1(1.0f, 2.0f, 3.0f);
+    T t2(2.0f, 3.0f, 4.0f);
+    //REQUIRE(t1.sum<0, 0>(t2) == (1.0f * 2.0f) + (2.0f * 3.0f) + (3.0f * 4.0f));
 }
