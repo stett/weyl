cd8dbb8b4c720725c4810e23512d34b39a933ef5
stett <stettjawa@gmail.com>
2017-05-26 15:12:46 -0700

    vector magnitude test

--- test_vector.cpp
+++ test_vector.cpp
@@ -1,4 +1,4 @@
-#include <type_traits>
+#include <cmath>
 #include "catch.hpp"
 #include "vector.h"
 using namespace weyl;
@@ -25,4 +25,14 @@ TEST_CASE("Vector cross product (mock - 2D)", "[vector]") {
     float product = cross(v1, v2);
     float expected_product = 4.0f - 6.0f;
     REQUIRE(product == expected_product);
+}
+
+TEST_CASE("Vector magnitude squared", "[vector]") {
+    vector<float, 2> v(1.0f, 2.0f);
+    REQUIRE(magnitude_sq(v) == 5.0f);
+}
+
+TEST_CASE("Vector magnitude", "[vector]") {
+    vector<float, 2> v(1.0f, 2.0f);
+    REQUIRE(magnitude(v) == std::sqrt(5.0f));
 }
