76dac945644b7be01556f9833f7d34d61a971a01
Stett <stettjawa@gmail.com>
2017-05-30 15:57:47 -0700

    add a spot to hook in test pass badge generation

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,18 @@
 // Catch
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
 #include "test_tensor.cpp"
 #include "test_vector.cpp"
 #include "test_matrix.cpp"
+
+int main( int argc, char* argv[] )
+{
+    // Run tests
+    int result = Catch::Session().run( argc, argv );
+
+    // TODO: Generate result badge
+    bool success = result == 0;
+
+    // Return the number of failed tests
+    return ( result < 0xff ? result : 0xff );
+}
